9846859df5ad9a6b2045892d87a8bde0d08667db
DCoppee <dominiquecoppee.pro@gmail.com>
2022-12-20 16:46:38 +0100

    CSS

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # random-quote-using-async-await
+
+Contexte: formation WebDev @BeCode
+
+## Exercice: consume a web service using async/await
+* Durée: 1 jour
+* Mode de travail: solo
+* Mission: publier une page web affichant une citation aléatoire issue d'un service Web.
--- script.js
+++ script.js
@@ -1,14 +1,10 @@
+//Publish a web page that shows a random quote from an available Web Service.
 let body = document.body;
 
-let btn = document.createElement("button");
-btn.innerHTML = "Another quote";
-body.appendChild(btn);
-btn.addEventListener('click', quoteCall);
-
 let blQuote = document.createElement("blockquote");
-let img = document.createElement("img");
-let author = document.createElement("p");
-
+    img = document.createElement("img");
+    author = document.createElement("p");
+ 
 body.appendChild(blQuote);
 body.appendChild(img);
 body.appendChild(author);
@@ -16,10 +12,11 @@ body.appendChild(author);
 async function quoteCall(){
     try{
         let response = await fetch('https://thatsthespir.it/api')
-        let json = await response.json()
+            json = await response.json()
         blQuote.innerHTML = json.quote;
-        img.innerHTML = json.photo;
         author.innerHTML = json.author;
+        img.setAttribute("src",json.photo);
+        img.setAttribute("alt",json.author);
     }
     catch(error) {
         alert(error);
@@ -27,4 +24,7 @@ async function quoteCall(){
 }
 quoteCall()
 
-        
+let btn = document.createElement("button");
+btn.innerHTML = "Another quote";
+body.appendChild(btn);
+btn.addEventListener('click', quoteCall);
